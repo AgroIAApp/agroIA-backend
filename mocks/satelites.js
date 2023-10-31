381a0885989d77b066041d54201dd6d69421839d
Ezequiel-H <horowitzeze@gmail.com>
2023-10-31 12:37:54 -0300

    removing middle problem

--- mocks/satelites.js
+++ mocks/satelites.js
@@ -3,12 +3,12 @@ const PROBLEMA_ABAJO = (index, size) =>
   index < size * 0.7 ? Math.random() * 0.15 + 0.85 : Math.random() * 0.15;
 const PROBLEMA_ARRIBA = (index, size) =>
   index < size * 0.3 ? Math.random() * 0.15 : Math.random() * 0.15 + 0.85;
-const PROBLEMA_MEDIO = (index, size) =>
-  index < size * 0.4
-    ? Math.random() * 0.15 + 0.85
-    : index < size * 0.6
-    ? Math.random() * 0.15
-    : Math.random() * 0.15 + 0.85;
+// const PROBLEMA_MEDIO = (index, size) =>
+//   index < size * 0.4
+//     ? Math.random() * 0.15 + 0.85
+//     : index < size * 0.6
+//     ? Math.random() * 0.15
+//     : Math.random() * 0.15 + 0.85;
 
 export const mockAddSateliteData = (plots) => {
   const valRandomIndex = Math.random();
@@ -28,7 +28,7 @@ export const mockAddSateliteData = (plots) => {
       ? PROBLEMA_ABAJO
       : valRandom < 0.75
       ? PROBLEMA_ARRIBA
-      : PROBLEMA_MEDIO;
+      : PROBLEMA_ARRIBA;
   return plots.map((plot, index) => ({
     color: Math.floor(Math.random() * 16777215).toString(16),
     ndmi:
